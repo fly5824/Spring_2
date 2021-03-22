af3005d27b059896ea1e0b41265f9eda557dbeca
uooh_Z <fly5148@naver.com>
2021-03-22 15:16:09 +0900

    c

--- src/main/java/com/naver/s2/robot/Arm_Right.java
+++ src/main/java/com/naver/s2/robot/Arm_Right.java
@@ -3,11 +3,9 @@ package com.naver.s2.robot;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component("ar")
+
 public class Arm_Right extends Arm {
 	
-	public Arm_Right() {
-		this.setName("Right Arm");
-	}
+
 
 }
